d439a46aeb8548a4a34575df44fa0c378e28f181
Claudia Cecilia Rossi <claudiarossi0707@gmail.com>
2021-09-04 17:11:23 -0300

    better

--- app.js
+++ app.js
@@ -93,7 +93,9 @@ app.use(function(err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render('MensajesAlUsuario',{
+    mensaje:err.message
+  });
 });
 
 module.exports = app;
