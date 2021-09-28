275c7cc650327f8fd6e2a8c1041bff2da394fcc2
Claudia Cecilia Rossi <claudiarossi0707@gmail.com>
2021-09-28 18:11:14 -0300

    better

--- routes/look.js
+++ routes/look.js
@@ -8,10 +8,7 @@ router.use(express.urlencoded({ extended: false }));
 // GET users listing.
 router.get('/', function(req, res, next) {
   
-  console.log('en LOOK.js session id usuario= ' + req.session.id_usuario );
-  console.log('en LOOK.js session usuario= ' + req.session.usuario );
-
-  res.render('look', {usuario:req.session.usuario,id_usuario:req.session.id_usuario});
+  res.render('look');
 
 });
 
