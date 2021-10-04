a716198e2edc329d7116277678ad63574d564ce1
Claudia Cecilia Rossi <claudiarossi0707@gmail.com>
2021-10-04 16:18:35 -0300

    better

---
+++ routes/descargar.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var router = express.Router();
+
+//probando descarga
+
+//var bodyParser = require('body-parser');
+router.use(express.urlencoded({ extended: false }));
+
+// GET users listing.
+router.get('/:archivo', function(req, res, next) {
+
+    var v_archivo = req.params.archivo;
+    console.log('en pdf.js con archivo= ' + v_archivo);
+  
+    res.download('/img/' + v_archivo,v_archivo);
+  
+  });
+
+
+module.exports = router;
+
+
