3ac7c80f6bf90f7b513d2062f6e5018d85ec75bf
Claudia Cecilia Rossi <claudiarossi0707@gmail.com>
2021-11-22 13:11:38 -0300

    modificaciones script bd

--- resume-ccr.sql
+++ resume-ccr.sql
@@ -1,15 +1,14 @@
 CREATE TABLE IF NOT EXISTS usuarios (
-`id_usuario` int(100) NOT NULL AUTO_INCREMENT,
+`id_usuario` int NOT NULL AUTO_INCREMENT,
 `usuario` varchar(20) NOT NULL,      
 `clave` varchar(20) NOT NULL,
 `nombre` varchar(50) NOT NULL,      
 `apellido` varchar(50) NOT NULL,          
 PRIMARY KEY (`id_usuario`),
-UNIQUE(usuario)   )
-
+UNIQUE(usuario)   );
 
 CREATE TABLE IF NOT EXISTS `items` (
-  `id_item` int(100) NOT NULL AUTO_INCREMENT,
+  `id_item` int NOT NULL AUTO_INCREMENT,
   `id_usuario` int(100) NOT NULL,
   `fecha_item` varchar(20) NOT NULL,
   `desc_item` varchar(255) NOT NULL,
